aa2f57318bfde1f77f572ff795a2f4ee69003f65
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-04-09 16:02:22 +0700

    Add project files.

---
+++ Exercise_2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'prodiTIDataSet.Mahasiswa' table. You can move, or remove it, as needed.
+            this.mahasiswaTableAdapter.Fill(this.prodiTIDataSet.Mahasiswa);
+
+            dataGridView1.Columns[0].DefaultCellStyle.ForeColor = Color.Purple;
+            dataGridView1.Columns[1].DefaultCellStyle.ForeColor = Color.Blue;
+            dataGridView1.Columns[2].DefaultCellStyle.ForeColor = Color.AliceBlue;
+            dataGridView1.Columns[3].DefaultCellStyle.ForeColor = Color.Olive;
+            dataGridView1.Columns[4].DefaultCellStyle.ForeColor = Color.Brown;
+            dataGridView1.Columns[5].DefaultCellStyle.ForeColor = Color.Yellow;
+
+            dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.Pink;
+            dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.Orange;
+            dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.DarkGreen;
+            dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.Blue;
+            dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.Maroon;
+            dataGridView1.Columns[5].DefaultCellStyle.SelectionBackColor = Color.Chocolate;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
